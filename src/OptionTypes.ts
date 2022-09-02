8364af032d2db0a2d102d24bfa93323a0939016f
franck-romano <franck.romano@protonmail.com>
2022-09-02 09:49:16 +0200

    deps: bump typescript to 4.8.2 and fix some security issues

--- src/OptionTypes.ts
+++ src/OptionTypes.ts
@@ -2,7 +2,6 @@ export type OrElseFn<TYPE> = () => TYPE;
 export type MapFn<TYPE, NEW_TYPE> = (value: TYPE) => NEW_TYPE;
 export type FlatMapFn<TYPE, NEW_TYPE> = (value: TYPE) => Optional<NEW_TYPE>;
 export type PeekFn<TYPE> = (value: TYPE) => void | Promise<unknown>;
-export type Nullable = undefined | null;
 
 export interface Optional<TYPE> {
   getOrElse(fallbackValue: TYPE | OrElseFn<TYPE>): TYPE;
